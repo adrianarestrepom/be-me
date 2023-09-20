70591a9be2347302bb33e9eda8814538b72f8b1e
Adriana Restrepo <adrianarestrepom@hotmail.com>
2023-09-19 19:48:05 -0500

    Creacion Adriana Project

---
+++ projects/be-chotas/Projecto Adriana/AdrianaProject/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='
+      flex
+      rounded-md
+      w-full
+      h-[22px]
+      p-0
+      m-auto
+      shadow-xl
+      shadow-slate-300
+      justify-center
+      items-center
+      md:p-40
+      md:bg-sky-700
+      lg:bg-red-400
+      dark:bg-green-400
+      '>
+        <h1>Hola Mundo</h1>
+        
+      </div>
+      <div className='portraid:hidden'>Me veo solo en portarid</div>
+    </>
+  )
+}
+
+export default App
